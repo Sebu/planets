b334c8725d0e9fecf51d5d67fa8bb9f431cc6ff5
Sebastian Szczepanski <millennium-x@gmx.net>
2010-10-31 19:05:52 +0100

    textures etc.

--- stardateviz.js
+++ stardateviz.js
@@ -1,21 +1,67 @@
 SceneJS.Spherical  = SceneJS.createNodeType("spherical");
+SceneJS.Planet  = SceneJS.createNodeType("planet");
+SceneJS.Circle  = SceneJS.createNodeType("circle", "geometry");
 
-SceneJS.Spherical.prototype._init = function(params) {
+SceneJS.Circle.prototype._init = function(params) {
+        // this.setDensity(params.density);
+     this._create = function() {
+		     
+		      return {
+		          primitive : "line-loop",
+		          positions : [
+						      -1.0,-1.0,0.0,
+		      				1.0,-1.0,0.0,
+		      				1.0,1.0,0.0,
+						      -1.0,1.0,0.0
+		
+		      		],
+		          indices : [ 0, 1, 2, 3 ]
+		      };
+	   };
+};
+
+
+SceneJS.Planet.prototype._init = function(params) {
         // this.setDensity(params.density);
 			  tmpNodes =  this.removeNodes();
         this.addNode( this._zRotate = SceneJS.rotate({angle: 22.0, z: 1.0}, 
 								        this._yRotate = SceneJS.rotate({angle: 45.0, y: 1.0},
+								        	SceneJS.material({              
+								        	baseColor: { r: 0.0, g: 0.0, b: 0.0 },
+						              specularColor:  { r: 0.0, g: 0.0, b: 0.0 },
+        						      emit: 0.0, specular: 0.0, shine: 1.0},
+								          	new SceneJS.Texture({ layers: [{	uri: params.tex }] }, new SceneJS.Sphere())
+								          )
+	        						  )
+	        						)
+	        					);
+        						
+        this._yRotate.addNodes(tmpNodes);
+};
+
+SceneJS.Planet.prototype.setRotate = function(angle) {
+		this._yRotate.setAngle(angle);
+};
+ 
+
+
+
+SceneJS.Spherical.prototype._init = function(params) {
+        // this.setDensity(params.density);
+			  tmpNodes =  this.removeNodes();
+        this.addNode( this._zRotate = SceneJS.rotate({angle: params.angle, z: 1.0}, 
+								        this._yRotate = SceneJS.rotate({angle: 45.0, y: 1.0},
 								              SceneJS.material({
-              baseColor:      { r: 0.2, g: 0.9, b: 0.2 },
-              specularColor:  { r: 0.9, g: 0.9, b: 0.2 },
-              emit: 0.0, specular: 0.9, shine: 6.0
+              baseColor:      { r: 1.0, g: 1.0, b: 0.5 },
+              specularColor:  { r: 0.0, g: 0.0, b: 0.0 },
+              emit: 0.0, specular: 0.0, shine: 1.0
            },
-		        								SceneJS.scale( {x:0.02, y:5.0, z:0.02 }, SceneJS.cube()),
-		        								SceneJS.scale( {x: params.scale, y: params.scale, z: params.scale }, SceneJS.material({
-              baseColor:      { r: 0.9, g: 0.2 * params.scale, b: 0.2 },
-              specularColor:  { r: 0.9, g: 0.9, b: 0.2 },
-              emit: 0.0, specular: 0.9, shine: 1.0, opacity: 0.2
-           }, SceneJS.sphere()))
+		        	SceneJS.scale( {x:0.05, y: 1.0 * params.scale, z:0.05 }, SceneJS.sphere()),
+		        	SceneJS.scale( {x: params.scale, y: params.scale, z: params.scale }, SceneJS.material({
+	              baseColor:      { r: 0.9 , g: 0.7, b: 0.2 },
+  	            specularColor:  { r: 0.0, g: 0.0, b: 0.0 },
+    	          emit: 0.0, specular: 0.0, shine: 1.0, opacity: 0.2
+           }, SceneJS.sphere(), new SceneJS.Circle() ))
 		        						)
         							)
         						));
@@ -25,16 +71,4 @@ SceneJS.Spherical.prototype._init = function(params) {
 
 SceneJS.Spherical.prototype.setRotate = function(angle) {
 		this._yRotate.setAngle(angle);
-};
- 
-
-SceneJS.Spherical.prototype._render = function(traversalContext) {
-    if (SceneJS._traversalMode == SceneJS._TRAVERSAL_MODE_PICKING) {
- 
-        /* Don't need fog for pick traversal
-         */
-        this._renderNodes(traversalContext);
-    } else {
-        this._renderNodes(traversalContext);
-    }
 };
