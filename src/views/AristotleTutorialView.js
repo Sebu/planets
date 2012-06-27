2c0ee863dcf15e5ead4f269b733c7c669fcd4055
Sebastian Szczepanski <millennium-x@gmx.net>
2012-06-27 16:05:57 +0200

    aristotle

--- src/views/AristotleTutorialView.js
+++ src/views/AristotleTutorialView.js
@@ -6,17 +6,23 @@ AristotleTutorialView.prototype = new AristotleView;
 AristotleTutorialView.prototype.constructor = AristotleTutorialView;
 
 
+
+AristotleTutorialView.prototype.showSpheres = function(model, list) {
+  var i=0;
+  for(i=1; i<=8; ++i) {
+    model["setShowSphere" + i](false); 
+  }
+
+  for(i=0; i<list.length; ++i) {
+    model["setShowSphere" + list[i]](true); 
+  }
+}
+
 AristotleTutorialView.prototype.switch1 = function(model) {
   model.setSpeed1(1);
-  model.setSpeed2(4380);
-  model.setSpeed3(390);
-  model.setShowSphere1(true);
-  model.setShowSphere2(true);
-  model.setShowSphere3(true);
-  model.setShowSphere4(true);
-  model.setShowSphere5(false);
-  model.setShowSphere8(false);
-//  model.setSpeed4(-390);
+  model.setSpeed2(100);
+  model.setSpeed3(100);
+  this.showSpheres(model, [1,2,3,4]);
 }
 
 AristotleTutorialView.prototype.switch2 = function(model) {
